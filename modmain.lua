ff16a22061143a731f19a9095397f2b475868c78
Sam <sammycolonna@gmail.com>
2021-10-11 23:31:26 -0400

    Added support for custom stories

--- modmain.lua
+++ modmain.lua
@@ -48,8 +48,8 @@ end
 -- HIGH LEVEL FUNCTIONS
 
 -- Parses this mod's config options and returns a list of story names that need to be registered
-local function GetStoryNamesToRegister()
-	local wpp_story_names = {}
+local function GetStoriesToRegister()
+	local wpp_stories = {}
 
 	-- Loop over the config options
 	debug_print("[Config] Parsing config options")
@@ -62,10 +62,23 @@ local function GetStoryNamesToRegister()
 
 			-- If this is a modded story and it is enabled
 			if has_prefix(option_name, MODDED_STORY_PREFIX) and option_value == true then
-				-- Add the story name to `wpp_story_names`
+				-- Add the story name and directory to `wpp_stories`
 				local modded_name = remove_prefix(option_name, MODDED_STORY_PREFIX):upper()
 				debug_print("[Config] Adding modded story: \"%s\" (%s)", option_name, modded_name)
-				table.insert(wpp_story_names, modded_name)
+				table.insert(wpp_stories, {
+					name = modded_name,
+					dir = MODDED_STORY_DIRECTORY
+				})
+			end
+
+			if has_prefix(option_name, OTHER_STORY_PREFIX) and option_value == true then
+				-- Add the story name and directory to `wpp_stories`
+				local modded_name = remove_prefix(option_name, OTHER_STORY_PREFIX):upper()
+				debug_print("[Config] Adding modded story: \"%s\" (%s)", option_name, modded_name)
+				table.insert(wpp_stories, {
+					name = modded_name,
+					dir = OTHER_STORY_DIRECTORY
+				})
 			end
 
 			-- If this is a vanilla story and it is disabled
@@ -78,23 +91,23 @@ local function GetStoryNamesToRegister()
 		end
 	end
 	
-	return wpp_story_names
+	return wpp_stories
 end
 
 -- Finds the lua file associated with each story in `stories` and registers it
 local function RegisterStories(stories)
 	debug_print("[Stories] Adding modded stories to registry")
-	for _, story_name in ipairs(stories) do
+	for _, story in ipairs(stories) do
 		-- Load the story and split it into lines
-		debug_print("[Stories] Loading story \"%s\" (%s)", story_name, STORY_DIRECTORY..story_name:lower())
-		local story_text = GLOBAL.require(STORY_DIRECTORY..string.lower(story_name)) -- :gsub("([.,:;?!]) ", "%1\n")
+		debug_print("[Stories] Loading story \"%s\" (%s)", story.name, STORY_DIRECTORY..story.dir)
+		local story_text = GLOBAL.require(STORY_DIRECTORY..story.dir) -- :gsub("([.,:;?!]) ", "%1\n")
 		local story_lines = split(story_text, "\n")
 
 		-- Load the story's line durations and text into `story`
 		debug_print("[Stories] Parsing story")
-		local story = {}
+		local story_formatted = {}
 		for index, line in ipairs(story_lines) do
-			story[index] = {
+			story_formatted[index] = {
 				duration = get_line_duration(line),
 				line = line,
 			}
@@ -102,15 +115,15 @@ local function RegisterStories(stories)
 
 		-- Register the story
 		debug_print("[Stories] Registering story")
-		GLOBAL.STRINGS.STORYTELLER.WALTER.CAMPFIRE[story_name] = { lines = story }
+		GLOBAL.STRINGS.STORYTELLER.WALTER.CAMPFIRE[story.name] = { lines = story_formatted }
 	end
 end
 
 -- MAIN FUNCTION
 
 local function script()
-	local wpp_story_names = GetStoryNamesToRegister()
-	RegisterStories(wpp_story_names)
+	local wpp_stories = GetStoriesToRegister()
+	RegisterStories(wpp_stories)
 end
 
 debug_print("[Init] Running script")
