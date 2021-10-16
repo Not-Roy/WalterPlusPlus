f3d557bb7836b1fa7f523a4ead03cdef0809b997
Sam <sammycolonna@gmail.com>
2021-10-15 22:59:05 -0400

    Rewrote the wendigo story

--- scripts/stories/wpp/wendigo.lua
+++ scripts/stories/wpp/wendigo.lua
@@ -1,6 +1,29 @@
 -- Inspired by Reddit Post "My friends and I think we were being hunted by a wendigo..." by user u/l1kmataint
 -- (https://www.reddit.com/r/WendigoStories/comments/fnsnsu/my_friends_and_i_think_we_were_being_hunted_by_a/)
 return [[
+It was a late Autumn night.
+I had just finished fishing,
+and started walking back to camp.
+I heard some dogs barking in the distance,
+and I was a little unnerved.
+Suddenly I feel a deep chill.
+A horrible, unnatural chill.
+I kept walking.
+Until Woby stopped dead in her tracks.
+She was staring at something.
+And then I saw it.
+A creature in the distance.
+This... thing...
+It looked like a skeleton wearing a suit of skin.
+but it had the horns of a moose.
+And its skin looked dead gray.
+Apparently I had stared too long,
+because it stopped and looked me dead in the eye.
+It mumbled something I couldn't understand,
+in a deep, demonic voice.
+Then it howled and ran out of sight on all fours.]]
+
+--[[
 It was a late Autumn night,
 and I was walking back to camp after a fishing trip.
 I was a little unnerved,
